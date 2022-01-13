66094549fa1a859bc3d91c2585bb11257ac49eea
LiuFangYu <905805357@qq.com>
2022-01-14 01:07:20 +0800

    初步搭建，信访数据可视化

---
+++ algorithm/request_extract.py
@@ -0,0 +1,98 @@
+import jieba
+import re
+import jieba.analyse
+import fool
+import json
+import csv
+import xlrd
+import xlwt
+
+
+# 判断是否以数字和字母结尾
+def end_num_or_character(string):
+    text = re.compile(r".*[0-9][A-Z][a-z]$")
+    if text.match(string):
+        return False
+    else:
+        return True
+
+#获取关键信息
+def get_keyinfo(s):
+    '''
+    input:  string
+    output： json-str （date\location\org\keywords）
+    '''
+    dic = {'date':[],'location':[],'org':[],'keywords':[]}
+    #时间正则表示
+    pattern = re.compile('\d{2,4}[\\/年]{1}\d{1,2}[\/月]{1}\d{1,2}[日号]{0,1}|\d{2,4}[\/年]{1}\d{1,2}[\/月]{1}|\d{1,2}[\/月]{1}\d{1,2}[日号]{0,1}|\d{1,2}[\/月]{1}|\d{4}[年]{1}')
+    dic['date'] = pattern.findall(s)
+    #keywords获取
+    keywords = jieba.analyse.extract_tags(s, topK=5, withWeight=True, allowPOS=())
+    dic['keywords'] = [keyword for keyword,score in keywords]
+    #location的获取
+    #org的获取
+    for info in fool.analysis(s)[1][0]:
+        if info[2] == 'org' or info[2] == 'company':
+            if info[3] not in dic['org']:#去除重复
+                dic['org'].append(info[3])
+        if info[2] == 'location':
+            if len(info[3])>1 and '甬' not in info[3] and end_num_or_character(info[3]) and info[3] not in['莱绅','伊利','中国']:
+                if info[3] not in dic[info[2]]:#去除重复
+                    dic['location'].append(info[3])
+    return dic
+
+# 获取用户需求
+def get_request_by_keyword(s):
+    keyword = jieba.analyse.extract_tags(s, topK=1, withWeight=True, allowPOS=())[0][0]
+    # 返回的是字符串
+    return [i for i in re.split('[，,.。]', s) if keyword in i][-1]
+
+#紧急词汇
+emergency_word = ['尽快', '马上', '及时', '盼复', '盼', '反映多次', '多次反映', '要求回复', '投诉', '多次询问', '严重', '催补',
+                  '抓紧', '加快', '再次处理', '再次反映', '再次来电', '反映过', '加急', '重新处理', '强制', '严重', '再次', '多次',
+                  '立马解决', '立刻', '立马', '立即', '刻不容缓', '迫不及待', '十万火急', '火烧眉毛', '迫在眉睫', '殷切', '遑急',
+                  '危殆', '紧要', '告急', '垂危', '危机', '急切', '遑急', '危急', '蹙迫', '火速', '要紧', '急迫', '紧迫', '火急',
+                  '迫切', '弁急', '急不可待', '急不可耐', '急不暇择']
+#划分紧急程度
+def emergency_degree_classification(content_text, key_word):
+    #是否紧急
+    flag = False
+    for word in key_word:
+        if word in content_text:
+            flag = True
+    return flag
+
+#诉求词汇，按频次排序
+request_word = ['要求', '反映', '处理', '投诉', '现来电', '来电要求', '请', '望', '解决', '建议', '来电投诉', '查处', '咨询',
+                '诉求', '举报', '请求', '核实', '来电咨询', '严重影响', '希望', '调查处理', '询问', '核查', '帮助', '为什么', '责令',
+                '来电表示', '请问', '请求帮助', '来电建议', '反应', '公示', '请帮忙', '恳请', '请核实', '盼', '来电询问',
+                '望有关部门', '市民投诉', '建议相关部门', '求助', '主要诉求', '来电求助', '恳求', '采取措施', '来访请求', '来电确认',
+                '来电感谢', '帮忙确认', '烦请采取措施']
+request_double_word = [['请', '处理'], ['要求', '处理']]
+#根据诉求关键词，匹配提取出诉求主句
+def get_request(content_text, key_word, key_double_word):
+    request = ''
+    content_list = content_text.split('，')
+    for word in key_word:
+        for content in content_list:
+            if word in content:
+                sub_content_list = content.split('。')
+                for sub_content in sub_content_list:
+                    if word in sub_content:
+                        return sub_content
+    for word in key_double_word:
+        for content in content_list:
+            if word[0] in content and word[1] in content:
+                return content
+    return request
+
+#获取词的频次
+def count_word(content_text, key_word, word_num):
+    for word in key_word:
+        if word in content_text:
+            word_num[word] += 1
+    return word_num
+
+#按字典value值排序
+def dict_sort_by_value(mydict, reverse=True):
+    return sorted(mydict.items(), key=lambda kv: (kv[1], kv[0]), reverse=reverse)
---
+++ static/js/function.js
@@ -0,0 +1,116 @@
+init_graph = function(graph, flag=true){
+    graph = JSON.parse(graph);
+
+    //给节点的属性进行操作
+    graph.nodes.forEach(function (node) {
+        node.symbolSize = 8;
+        // node.value = node.symbolSize;
+        if(flag == true)
+            node.category = node.attributes.modularity_class;
+        node.draggable = true;
+    });
+    return graph;
+};
+
+init_option = function(graph){
+    var option;
+    var categories = [];//设置节点属性
+    categories[0] = {
+            name: '未激活节点',
+            itemStyle: {
+                color: '#426ab3',
+                opacity: 0.3, //不透明度
+            }
+        }
+        categories[1] = {
+            name: '激活节点',
+            itemStyle: {
+                color: '#ef4136',
+                opacity: 1, //不透明度
+            },
+            label: {
+                fontSize: 20,
+            },
+
+        };
+    //设置要生成的图的相关属性
+    option = {
+        title: {
+            text: '',
+            subtext: '',
+            top: 'top',
+            left: 'left'
+        },
+        tooltip: {},
+        legend: [{
+            // selectedMode: 'single',
+            data: categories.map(function (a) {
+                return a.name;
+            })
+        }],
+        animationDuration: 1500,
+        animationEasingUpdate: 'quinticInOut',
+        series: [
+            {
+                name: 'Les Miserables',
+                type: 'graph',
+                layout: 'none',
+                //下面三项分别设置节点数据，边数据，种类数据
+                data: graph.nodes,
+                links: graph.links,
+                categories: categories,
+                roam: true,
+                animation: false,
+                focusNodeAdjacency: true,
+                itemStyle: {
+                    normal: {
+                        borderColor: '#fff',
+                        borderWidth: 1,
+                        shadowBlur: 10,
+                        shadowColor: 'rgba(0, 0, 0, 0.3)'
+                    }
+                },
+                label: {//设置节点标签
+                    position: 'right',
+                    formatter: '{b}'
+                },
+                lineStyle: {//边的种类
+                    color: '#9b95c9',
+                    curveness: 0.3
+                },
+                emphasis: {
+                    lineStyle: {
+                        width: 10
+                    }
+                },
+                force: {
+                    repulsion: 270
+                }
+            }
+        ]
+    };
+    return option;
+}
+function getUrlParam(name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+            var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+            if (r != null) return unescape(r[2]);
+            return null; //返回参数值
+        }
+redirection = function()  {
+    let userName = getUrlParam("userName");
+    $.ajax({
+        type: "POST",
+        url: "/fun",
+        data: {
+            "userName": userName
+        },
+        success: function(data) {
+            if(data.userInfo == 1)
+                $(window).attr('location','/login');
+        },
+        error: function() {
+            console.log('error');
+        }
+    })
+}
